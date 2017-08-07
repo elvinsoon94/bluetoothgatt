87b9e7c680f0041d275830d1ebeca37105327cfb
elvinsoon94 <elvinsoon94@gmail.com>
2017-08-07 15:21:06 +0800

    Add files via upload

--- app/src/main/java/com/example/nkwm87/bluetoothgatt/DeviceControlActivity.java
+++ app/src/main/java/com/example/nkwm87/bluetoothgatt/DeviceControlActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,34 +17,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ExpandableListAdapter;
+import android.widget.CompoundButton;
 import android.widget.ExpandableListView;
 import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
 
 public class DeviceControlActivity extends AppCompatActivity {
-    private final static String TAG = "Bluetooth Activity";
+    private final static String TAG = "BluetoothActivity";
 
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
 
     private TextView mConnectionState;
     private TextView mDataField;
-    private Button showUpTimeButton;
     private BluetoothLeService mBluetoothLeService;
     private ExpandableListView mGattServicesList;
     private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics = new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
     private String mDeviceName;
     private String mDeviceAddress;
     private boolean mConnected = false;
+    private ConstraintLayout messageContainer;
+    private ConstraintLayout mLedSwitch;
+    private int writeMessage = 0;
+    private int ledSwitch = 0;
+    private int ledon = 0;
+    private int ledoff = 0;
+    private ToggleButton LEDswitch;
 
     byte[] info_Data = null;
 
@@ -85,28 +91,211 @@ public class DeviceControlActivity extends AppCompatActivity {
                 invalidateOptionsMenu();
                 clearUI();
             }else if(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)){
-                //displayGattServices(mBluetoothLeService.getSupportedGattServices());
+                displayGattServices(mBluetoothLeService.getSupportedGattServices());
             }else if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)){
-                //DisplayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+                DisplayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
             }
 
         }
     };
 
-    /*private final ExpandableListView.OnChildClickListener servicesListClickListener = new ExpandableListView.OnChildClickListener() {
+    private final ExpandableListView.OnChildClickListener servicesListClickListener = new ExpandableListView.OnChildClickListener() {
+
+        /** value for the charapop 2=read 8=write 10=read and write **/
+
         @Override
         public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
             if(mGattCharacteristics != null){
+                final boolean switchIson = true;
                 final BluetoothGattCharacteristic characteristic = mGattCharacteristics.get(groupPosition).get(childPosition);
                 final int charaProp = characteristic.getProperties();
-                if ((charaProp | BluetoothGattCharacteristic.PERMISSION_READ) > 0 ){
+                final String ledUUID = characteristic.getUuid().toString();
+                Log.d(TAG, "The UUID detected is:" +ledUUID);
+
+                if (ledUUID.equals("00002a00-0000-1000-8000-00805f9b34fb")){
+                    Log.d(TAG, "Selection of the Service");
+
+                    if(writeMessage==1) {
+                        messageContainer.setVisibility(View.INVISIBLE);
+                    }
+                    if(ledSwitch==1){
+                        mLedSwitch.setVisibility(View.INVISIBLE);
+                    }
+
+                    /*final ConstraintLayout mSelection = (ConstraintLayout)findViewById(R.id.functionSelectionLayout);
+                    mSelection.setVisibility(View.VISIBLE);
+
+                    Button mGPSButton = (Button)findViewById(R.id.gpsSelection);
+                    mGPSButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mSelection.setVisibility(View.INVISIBLE);
+                            Log.d(TAG, "GPS service is chosen");
+                            if(ledSwitch==1){
+                                mLedSwitch.setVisibility(View.INVISIBLE);
+                            }
+
+                            messageContainer = (ConstraintLayout)findViewById(R.id.message_container);
+                            messageContainer.setVisibility(View.VISIBLE);
+                            writeMessage = 1;
+                            Button sendData = (Button)findViewById(R.id.sendButton);
+                            sendData.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    final TextView message = (TextView)findViewById(R.id.dataMessage);
+                                    characteristic.setValue(message.getText().toString().getBytes());
+                                    Log.d(TAG, "The apps is writing the characteristics");
+                                    mBluetoothLeService.writeCharacteristic(characteristic);
+                                    message.setText("");
+                                    Log.d(TAG, "New Data is Written:" +characteristic);
+                                }
+                            });
+                        }
+                    });*/
+
+                    //Button mLEDButton = (Button)findViewById(R.id.ledSelection);
+                    //mLEDButton.setOnClickListener(new View.OnClickListener() {
+                        //@Override
+                        //public void onClick(View v) {
+                            //mSelection.setVisibility(View.INVISIBLE);
+                            Log.d(TAG, "LED Switch Service is Chosen");
+                            /*if(writeMessage==1) {
+                                messageContainer.setVisibility(View.INVISIBLE);
+                            }
+
+                            if (ledon==1 && ledoff==0){
+                                characteristic.setValue("ON");
+                                mBluetoothLeService.writeCharacteristic(characteristic);
+                            }
+                            if (ledoff==1 && ledon==0){
+                                characteristic.setValue("OFF");
+                                mBluetoothLeService.writeCharacteristic(characteristic);
+                            }*/
+
+                            mLedSwitch = (ConstraintLayout)findViewById(R.id.LED_Switch);
+                            mLedSwitch.setVisibility(View.VISIBLE);
+
+                            ledSwitch = 1;
+
+                            ToggleButton LEDswitch = (ToggleButton)findViewById(R.id.LEDSwitchButton);
+                            mBluetoothLeService.readCharacteristic(characteristic);
+
+                            /** Checking the condition of the LED before adding additional command **/
+                            String status;
+                            byte[] LEDdata = characteristic.getValue();
+
+                            if (LEDdata != null && LEDdata.length > 0){
+                                final StringBuilder stringBuilder = new StringBuilder(LEDdata.length);
+                                for (byte byteChar : LEDdata)
+                                    stringBuilder.append(String.format("%02X", byteChar));
+                                status = stringBuilder.toString();
+                                String printstatus = convertHexToString(status);
+                                Log.d(TAG, "The status of the LED is**" +printstatus);
+
+                                if(printstatus.equals("ON")){
+                                    Log.d(TAG, "The LED is already turned on");
+                                    LEDswitch.setChecked(switchIson);
+                                }
+                            }
+
+                            LEDswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                                @Override
+                                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                    if (isChecked){
+                                        Log.d(TAG, "LED is turned on");
+                                        characteristic.setValue("ON");
+                                        mBluetoothLeService.writeCharacteristic(characteristic);
+                                        ledon = 1;
+                                        ledoff = 0;
+                                    }else{
+                                        Log.d(TAG, "LED is turned off");
+                                        characteristic.setValue("OFF");
+                                        mBluetoothLeService.writeCharacteristic(characteristic);
+                                        ledon = 0;
+                                        ledoff = 1;
+                                    }
+                                }
+                            });
+
+                        //}
+                    //});
+
+                }else{
+
+                /** Checking the properties value of the characteristics **/
+
+                if (charaProp < 5 ){
+                    if(writeMessage==1) {
+                        messageContainer.setVisibility(View.INVISIBLE);
+                    }
+                    else if(ledSwitch==1){
+                        mLedSwitch.setVisibility(View.INVISIBLE);
+                    }
+
+                    Log.d(TAG,"Permission to read");
                     mBluetoothLeService.readCharacteristic(characteristic);
+                    writeMessage = 0;
                 }
+
+                else if (charaProp < 9){
+                    if(ledSwitch==1){
+                        mLedSwitch.setVisibility(View.INVISIBLE);
+                    }
+                    Log.d(TAG,"Permission to write");
+
+                    messageContainer = (ConstraintLayout)findViewById(R.id.message_container);
+                    messageContainer.setVisibility(View.VISIBLE);
+                    writeMessage = 1;
+                    Button sendData = (Button)findViewById(R.id.sendButton);
+                    sendData.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            final TextView message = (TextView)findViewById(R.id.dataMessage);
+                            characteristic.setValue(message.getText().toString().getBytes());
+                            Log.d(TAG, "The apps is writing the characteristics");
+                            mBluetoothLeService.writeCharacteristic(characteristic);
+                            message.setText("");
+                            Log.d(TAG, "New Data is Written:" +characteristic);
+                        }
+                    });
+                }
+
+                else if (charaProp > 9){
+                    if(ledSwitch==1){
+                        Log.d(TAG, "LED Switch layout is turned off");
+                        mLedSwitch.setVisibility(View.INVISIBLE);
+                    }
+                    Log.d(TAG,"Permission to read and write");
+
+
+                    messageContainer = (ConstraintLayout)findViewById(R.id.message_container);
+                    messageContainer.setVisibility(View.VISIBLE);
+                    writeMessage = 1;
+                    Button sendData = (Button)findViewById(R.id.sendButton);
+                    sendData.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            final TextView message = (TextView)findViewById(R.id.dataMessage);
+                            characteristic.setValue(message.getText().toString().getBytes());
+                            Log.d(TAG, "The apps is writing the characteristics");
+                            mBluetoothLeService.writeCharacteristic(characteristic);
+                            message.setText("");
+                        }
+                    });
+
+                    Button readData = (Button)findViewById(R.id.readButton);
+                    readData.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mBluetoothLeService.readCharacteristic(characteristic);
+                        }
+                    });
+                }}
                 return true;
             }
             return false;
         }
-    };*/
+    };
 
     private void updateConnectionState(final int resourceId) {
         runOnUiThread(new Runnable() {
@@ -120,6 +309,13 @@ public class DeviceControlActivity extends AppCompatActivity {
     private void clearUI(){
         mGattServicesList.setAdapter((SimpleExpandableListAdapter) null);
         mDataField.setText(R.string.no_data);
+        if(writeMessage == 1){
+            messageContainer.setVisibility(View.INVISIBLE);
+        }
+        if(ledSwitch == 1){
+            mLedSwitch.setVisibility(View.INVISIBLE);
+        }
+
     }
 
     @Override
@@ -139,32 +335,14 @@ public class DeviceControlActivity extends AppCompatActivity {
         mConnectionState = (TextView)findViewById(R.id.device_status);
         mDataField = (TextView)findViewById(R.id.device_data);
 
-        //mGattServicesList = (ExpandableListView)findViewById(R.id.gatt_service_list);
-        //mGattServicesList.setOnChildClickListener(servicesListClickListener);
 
-        Button showUpTimeButton = (Button)findViewById(R.id.showUptimeButton);
-        showUpTimeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayDeviceUpTimeInfo();
-            }
-        });
+        mGattServicesList = (ExpandableListView)findViewById(R.id.gatt_service_list);
+        mGattServicesList.setOnChildClickListener(servicesListClickListener);
+
         Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
         bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
     }
 
-    private void displayDeviceUpTimeInfo() {
-        List<BluetoothGattService> list = mBluetoothLeService.getSupportedGattServices();
-        BluetoothGattService service = list.get(2);
-        BluetoothGattCharacteristic info = service.getCharacteristic(UUID.fromString("ff51b30e-d7e2-4d93-8842-a7c4a57dfb08"));
-        mBluetoothLeService.readCharacteristic(info);
-        byte[] infoValue = info.getValue();
-        if(infoValue != null){
-            info_Data = infoValue;
-            String data1 = new String(info_Data);
-            mDataField.setText(data1);
-        }
-    }
 
     @Override
     protected void onResume(){
@@ -270,4 +448,19 @@ public class DeviceControlActivity extends AppCompatActivity {
         intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
         return intentFilter;
     }
+
+    public String convertHexToString(String hex){
+        StringBuffer sb = new StringBuffer();
+        StringBuilder temp = new StringBuilder();
+
+        for (int i =0; i<hex.length()-1; i+=2){
+            String output = hex.substring(i, (i + 2));
+            int decimal = Integer.parseInt(output, 16);
+            sb.append((char)decimal);
+
+            temp.append(decimal);
+        }
+        return sb.toString();
+    }
+
 }
